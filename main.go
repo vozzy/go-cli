b76c2fae0f2579b9ce8b7d333a79f67b4ca57c9a
Vernon Osborne <vosborne@dearx.co.za>
2022-11-13 23:32:29 +0200

    init TRI CLI

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 vozzy
+
+*/
+package main
+
+import "github.com/vozzy/tri/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# TRI CLI
+
+```
+cobra-cli init . -a "vozzy"
+go build
+```
